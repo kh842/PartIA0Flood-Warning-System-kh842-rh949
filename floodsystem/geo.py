3ecbc59329f0e13e19f4d44772d50c393c4a2b2f
kh842 <kh842@cam.ac.uk>
2026-02-02 14:34:15 +0000

    added 1B function

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -7,3 +7,11 @@ geographical data.
 """
 
 from .utils import sorted_by_key  # noqa
+from haversine import haversine
+
+def stations_by_distance(stations, p):
+    station_list = []
+    for station in stations:
+        distance = haversine(p, station.coord)
+        station_list.append(station, distance)
+    return sorted_by_key(station,1)
