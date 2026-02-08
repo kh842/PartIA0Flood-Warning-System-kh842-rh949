68456d22f632cb9072dadd3cccd1d078efd4e92e
Rayhan <rh949@cam.ac.uk>
2026-02-08 00:14:00 +0000

    Added demonstartion program  for part D

--- Task1D.py
+++ Task1D.py
@@ -13,6 +13,22 @@ for river in stations_list:
     if river not in river_list_norep:
         river_list_norep.append(river)
 
+river_list_norep.sort()
+
 #rivers_with_station(stations)
+
+print(str(len(river_list_norep)) + " stations"+ " The first 10 are "+ str(river_list_norep[:9]))
+
+
+dick= stations_by_river(stations)
+test = dick.get("River Aire")
+test.sort()
+print("The stations in River Aire are "+ str(test))
+test = dick.get("River Cam")
+test.sort()
+print("The stations in River Cam are "+ str(test))
+test = dick.get("River Thames")
+test.sort()
+print("The stations in River Thames are "+ str(test))
 #print(river_list_norep)
-print(stations_by_river(stations))
+#print(stations_by_river(stations))
