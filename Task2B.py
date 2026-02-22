55520b3c9e58d2fab616459074d5cb3c78cb2773
Rayhan <rh949@cam.ac.uk>
2026-02-22 00:56:04 +0000

    2B done

--- Task2B.py
+++ Task2B.py
@@ -1,10 +1,14 @@
 from floodsystem.stationdata import build_station_list
 from floodsystem.station import stations_level_over_threshold
+from floodsystem.stationdata import build_station_list, update_water_levels
 
 
-stations = build_station_list()
+def run():
+    stations = build_station_list()
+    update_water_levels(stations)
+    hi= stations_level_over_threshold(stations,0.8)
+    print(hi)
 
-
-
-hi= stations_level_over_threshold(stations,0)
-print(hi)
+if __name__ == "__main__":
+    print("*** Task 2B: CUED Part IA Flood Warning System ***")
+    run()
--- floodsystem/station.py
+++ floodsystem/station.py
@@ -7,6 +7,7 @@ for manipulating/modifying station data
 """
 
 import floodsystem.datafetcher as datafetcher
+#from floodsystem.stationdata import update_water_levels
 
 
 class MonitoringStation:
@@ -57,50 +58,19 @@ class MonitoringStation:
 
 
 
-    
-        
-    """def relative_water_level(self):
-        This method gives the water level as a number between 0 and compared to the relative range
-        latest_level = datafetcher.fetch_latest_water_level_data(self)
-        #past_water_level=datafetcher.fetch_measure_levels(self, 100)
-        
-        #water_level =past_water_level[1]
-        low = self.typical_range[0]
-        high = self.typical_range[1]
-        #high=max(water_level)
-        #low=min(water_level)
-
-        #relative= (float(latest_level)-float(low))/(high-low)
-        #self.typical_range = typical_range
-
-        #highest typical is 1 lowest is 0, the calc we need to do is current minus lowest water level/divided by range
-
-        return latest_level"""
-    
+    #2b    
     def relative_water_level(self):
-
-        # Check if typical_range is valid
+        if self.latest_level is None:
+            return None
         if not self.typical_range_consistent():
             return None
-        water_levels=datafetcher.fetch_latest_water_level_data()["items"]
-        # print(self.latest_level)
-        for item in water_levels:
-            if item["station"] == self.station_id:
-                if "latestReading" not in item.keys():
-                    return None
-                else:
-                    self.latest_level = item["latestReading"]["value"]
-        #hi=datafetcher.fetch_latest_water_level_data(self)
 
-        # Extract low/high values
-        low = self.typical_range[0]
-        high = self.typical_range[1]
+        low, high = self.typical_range
 
+        if high - low == 0:
+            return None
 
-        # Calculate relative level
-        relative_level = (self.latest_level - low) / (high - low)
-
-        return relative_level
+        return (self.latest_level - low) / (high - low)
   
 
      
@@ -112,8 +82,16 @@ def stations_level_over_threshold(stations, tol):
     for i in range(len(stations)):
         new_tuple=(stations[i].name,stations[i].relative_water_level())
         river_tuple.append(new_tuple)
+    sorted_river = sorted(river_tuple, key=lambda x: float('-inf') if x[1] is None else x[1], reverse=True)
+    filtered = [t for t in sorted_river if t[1] is not None]
+    final=[]
+    for  i in range(len(filtered)):
+     if filtered[i][1]>tol:
+         final.append(filtered[i])
 
-    return river_tuple
+    
+
+    return final
 
 
 def inconsistent_typical_range_stations(stations):
@@ -126,12 +104,12 @@ def inconsistent_typical_range_stations(stations):
 #2C
 def stations_highest_rel_level(stations, N):
 
-    list=[]
+    river=[]
     for i in range(len(stations)):
         new_tuple=(stations[i].name,stations[i].relative_water_level())
         list.append(new_tuple)
-    sorted_list = sorted(list, key=lambda x: x[1], reverse=True)
+    sorted_river = sorted(river, key=lambda x: float('-inf') if x[1] is None else x[1], reverse=True)
 
-    top_N=sorted_list[:N]
+    top_N=sorted_river[:N]
     return top_N   
 
